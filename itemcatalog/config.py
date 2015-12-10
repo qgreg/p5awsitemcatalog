679c910470279090e98ccfd249906cd6b7e6b809
Greg Quinlan <quinlangl@gmail.com>
2015-12-10 11:06:06 -0500

    fix: Turn off sqlalchemy change tracking

--- itemcatalog/config.py
+++ itemcatalog/config.py
@@ -8,6 +8,9 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 # Define the database - we are working with
 SQLALCHEMY_DATABASE_URI = 'postgres://ryztryqknsyzog:fVxpW9KcpmHAFAqMo1mBcidICf@ec2-107-21-219-109.compute-1.amazonaws.com:5432/d4kcqmr928j0p2'  # noqa
 
+# Turn off tracking modifications of objects and emiting signals
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 # Enable protection agains *Cross-site Request Forgery (CSRF)*
 CSRF_ENABLED = True
 
